217dc70e71cba4e00884481b046429bef25c0d40
Yuni-Liao <yuni70217@gmail.com>
2023-08-11 18:26:39 +0800

    update about us banner and tasty-food menu

--- js/food.js
+++ js/food.js
@@ -1,3 +1,4 @@
+// 啤酒區
 function adjustLayout() {
     let beerItems = document.querySelectorAll('.beer');
     let nextButton = document.querySelector('.fa-arrow-right-long');
@@ -39,8 +40,9 @@ window.onload = adjustLayout;
 window.onresize = adjustLayout;
 
 
-// 手機菜單彈窗
+// 菜單區
 $(function () {
+    // 手機菜單彈窗
     $('.phonemenubtn').click(function (e) {
         e.preventDefault();
         let content = '';
@@ -60,5 +62,55 @@ $(function () {
 
     $('.close-box').click(function () {
         $('.box').fadeOut();
+    });
+
+
+    // 電腦菜單翻閱
+    const frontPage = $("#front");
+    const pages = $(".page");
+    const pageone = $("#pageone")
+    const pagetwo = $("#pagetwo");
+
+    for (var i = 0; i < pages.length; i++) {
+        var page = pages[i];
+        if (i % 2 === 0) {
+            $(page).css("z-index", pages.length - i);
+        }
+    }
+
+    frontPage.click(function () {
+        var pageNum = $(this).index() + 1;
+        if (pageNum % 2 === 0) {
+            $(this).removeClass("flipped");
+            $(this).prev().removeClass("flipped");
+            pagetwo.css("opacity", 0);
+
+            pageone.css({
+                "opacity": 0,
+                "transition": "opacity 0.3s 1.5s"
+            });
+        } else {
+            $(this).addClass("flipped");
+            $(this).css("opacity", 0);
+            $(this).prev().removeClass("flipped");
+            $(this).next().css("opacity", 1);
+            pageone.css({
+                "opacity": 1,
+                "transition": "opacity 0.3s 1.5s"
+            });
+            pagetwo.css("opacity", 1);
+        }
+    });
+
+    $("#offmenu").click(function (e) {
+        e.preventDefault();
+        pageone.addClass("closemenu");
+        frontPage.removeClass("flipped");
+        frontPage.css("opacity", 1);
+        pageone.css({
+            "opacity": 0,
+            "transition": "opacity 0.2s"
+        });
+        pagetwo.css("opacity", 0);
     });
 })
